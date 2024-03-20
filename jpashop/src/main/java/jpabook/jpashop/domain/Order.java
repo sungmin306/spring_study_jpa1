94465f293c733bd3e19506f33f690713d9de9c4e
sungmin306 <another0306@naver.com>
2024-03-20 14:18:06 +0900

    JPA 강의 4일차

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Access;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,13 +17,16 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
 @Getter
 @Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -58,4 +62,50 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    protected Order() { // 생성해서 setter로 값을 입력하는 것을 막고 createOrder를 이용하게끔 진행
+                        // -> 왜냐하면 createOrder도 쓰고 생성자 이용해서 값 넣는것도 쓰면 유지보수 어려움
+                        // @NoArgsConstructor(access = AccessLevel.PROTECTED) 같은 의미
+
+    }
+
+    //==생성 메서드==// 복잡한 것은 별도의 생성 메서드가 필요함
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
+
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderStatus.java
@@ -1,5 +1,5 @@
 package jpabook.jpashop.domain;
 
 public enum OrderStatus {
-    OEDER, CANCEL
+    ORDER, CANCEL
 }
